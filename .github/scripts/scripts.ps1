99313329bfcdad7df27709daac23e2c17dcaad36
Manish Kutar <44708071+ManishKutar@users.noreply.github.com>
2022-01-30 20:01:22 +0800

    Update scripts.ps1

--- .github/scripts/scripts.ps1
+++ .github/scripts/scripts.ps1
@@ -1,2 +1,22 @@
 Write-Host 'Starting with BC Container installation on Windows.'
 Install-Module BcContainerHelper -force
+
+$ContainerName = 'ContainerName'
+
+# Image
+$artifactUrl = Get-BCArtifactUrl -country "au" -version 19
+$imageName = 'Saas19'
+
+# User
+$UserName = 'User'
+$Password = ConvertTo-SecureString "Pass123" -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ($UserName, $Password)
+			
+New-BCContainer -accept_eula `
+                -accept_outdated `
+                -updateHosts `
+                -containername $containername `
+   		          -artifactUrl $artifactUrl `
+                -imageName $imageName `
+                -auth NavUserPassword `
+                -Credential $credential
